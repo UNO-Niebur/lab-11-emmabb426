6a961a2dd9dffc44c4b50999cdc67cc811ee3848
emmabb426 <ebarnes17@unomaha.edu>
2026-04-15 14:50:36 +0000

    finished

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # StopLightSim.py
-# Name:
-# Date:
-# Assignment:
+# Name: Emma Barnes
+# Date: 04/14/2026
+# Assignment: Lab 11 - Traffic Light Simulation
 
 import simpy
 
@@ -33,6 +33,8 @@ def car(env, car_id):
 
     # TODO: Make the car wait while the light is red
     # Hint: use a loop and env.timeout(1)
+    while greenLight == False:
+        yield env.timeout(1)
 
     print("Car", car_id, "departed at", env.now)
 
@@ -47,7 +49,7 @@ def carArrival(env):
         print("Creating Car", car_id)
 
         # TODO: Start a new car process
-
+        env.process(car(env, car_id))
         yield env.timeout(5)
 
 
@@ -58,7 +60,9 @@ def main():
     env.process(stopLight(env))
     
     # TODO: Start the carArrival process
-
+    env.process(carArrival(env))
+    env.process(stopLight(env))
+    
     # Run simulation
     env.run(until=100)
 
